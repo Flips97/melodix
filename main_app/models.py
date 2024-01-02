f0f6d568524fe3a30f3be632e32a3374634bacb7
felipe97 <felipe.turen@gmail.com>
2024-01-02 15:36:39 -0800

    adds songs and playlist models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import date
+from django.urls import reverse
 # Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('songs_detail', kwargs={'pk': self.id})
+
+class Playlist(models.Model):
+    name =  models.CharField(max_length=100)
+    songs = models.ManyToManyField(Song)
+    user_favorite = models.ManyToManyField(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_playlist_set')
+
+    
+    
+    
+    
