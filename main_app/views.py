c4ff17fceee6808886ca2536dd31697c49adc650
tworem23 <josh.remington1997@gmail.com>
2024-01-02 18:56:39 -0700

    debugs import .models

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from models import Playlist, Song
+from .models import Playlist, Song
 
 # Create your views here.
 def home(request):
