01d8aa7957eb3532cdc81ac26058841714b93991
Melanie Winter <melaniebucher830@gmail.com>
2024-01-02 22:34:15 -0800

    Adds styling to playlists index page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Song, Playlist
 
-# Register your models here.
+
+# Register your models her
+admin.site.register(Song)
+admin.site.register(Playlist)
